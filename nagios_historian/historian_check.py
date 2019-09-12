cd7e2b6683a32f4504835525bf3cc97f69de01c1
matiasgrana <matias.grana@visitor.upm.com>
2019-09-12 09:48:10 -0300

    Se deshabilitan warnings de chequeo Https

--- nagios_historian/historian_check.py
+++ nagios_historian/historian_check.py
@@ -4,6 +4,10 @@ import requests
 import json
 import arrow
 import urllib3
+#Disable warnings HTTPS verification
+urllib3.disable_warnings()
+
+#urllib3.exceptions.InsecureRequestWarning
 
 # Return codes expected by Nagios
 OK = 0
@@ -48,12 +52,11 @@ class HistorianChecks:
         self.STATUS_WS_MAXIMUM_SESSIONS = '{}'.format(self.instance) + '.STATUS_WS_MAXIMUM_SESSIONS.F_CV'
         self.STATUS_WS_SERVICE = '{}'.format(self.instance) + '.STATUS_WS_SERVICE.F_CV'
         self.STATUS_LAST_UPDATE = '{}'.format(self.instance) + '.STATUS_LAST_UPDATE.A_CV'
-		#Disable warnings HTTPS verification
-		urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 		
     def get_tags_data(self):
     
-		
+
         #headers = {'accept': 'application/json'}
         header_token = {"Authorization": "Bearer {}".format(self.access_token)}
 
@@ -69,6 +72,7 @@ class HistorianChecks:
 
         #Vars
         retrcode = OK
+		
 
         #Create tuple with json and status code
         historian_tuple = self.get_tags_data()
